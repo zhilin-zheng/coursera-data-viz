253f44c79ac5169bd47c8070ea04f058c662fd52
Zhilin Zheng <zhilin.zheng@ibm.com>
2021-05-28 14:58:46 +0200

    correct line breaks

--- flight_details.py
+++ flight_details.py
@@ -16,25 +16,25 @@ airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.
 app = dash.Dash(__name__)
 
 # Build dash app layout
-app.layout = html.Div(children=[ html.H1('Flight Delay Time Statistics', style={'textAlign': 'center',
-                                    'color': '#503D36', 'font-size': 30}),
-                                html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010',
-                                type='number', style={'height': '35px', 'font-size': 30})],
-                                style={'font-size': 30}),
-                                html.Br(),
-                                html.Br(), 
-                                html.Div([
-                                        html.Div(dcc.Graph(id='carrier-plot')),
-                                        html.Div(dcc.Graph(id='weather-plot'))
-                                ], style={'display': 'flex'}),
+app.layout = html.Div(children=[ html.H1('Flight Delay Time Statistics',
+    style={'textAlign': 'center', 'color': '#503D36', 'font-size': 30}),
+    html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010',
+    type='number', style={'height': '35px', 'font-size': 30})],
+    style={'font-size': 30}),
+    html.Br(),
+    html.Br(), 
+    html.Div([
+    html.Div(dcc.Graph(id='carrier-plot')),
+    html.Div(dcc.Graph(id='weather-plot'))
+    ], style={'display': 'flex'}),
 
-                                html.Div([
-                                        html.Div(dcc.Graph(id='nas-plot')),
-                                        html.Div(dcc.Graph(id='security-plot'))
-                                ], style={'display': 'flex'}),
+    html.Div([
+    html.Div(dcc.Graph(id='nas-plot')),
+    html.Div(dcc.Graph(id='security-plot'))
+    ], style={'display': 'flex'}),
 
-                                html.Div(dcc.Graph(id='late-plot'), style={'width':'65%'})
-                                ])
+    html.Div(dcc.Graph(id='late-plot'), style={'width':'65%'})
+    ])
           
 """ Compute_info function description
 
